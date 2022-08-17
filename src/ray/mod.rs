5577afa4e7dbb3144f6472f6fac403228356bffc
Sam <git@samueldgv.com>
2022-08-17 20:07:35 +0200

    Chapter 6

--- src/ray/mod.rs
+++ src/ray/mod.rs
@@ -1,6 +1,8 @@
 use crate::color::Color;
 use crate::point::Point;
 use crate::vector::Vec3;
+use crate::hittable::Hittable;
+use crate::hittable::HitRecord;
 
 pub struct Ray {
     pub orig: Point,
@@ -12,21 +14,34 @@ impl Ray {
         self.orig + (self.dir * t)
     }
 
-    pub fn ray_color(&self) -> Color {
-        if self.hits_sphere(
-            Point {
-                x: 0.0,
-                y: 0.0,
-                z: -1.0,
-            },
-            0.5,
-        ) {
-            return Color {
-                x: 1.0,
-                y: 0.0,
-                z: 0.0,
-            };
+    fn hits_any_object(&self, objects : &Vec<Box<dyn Hittable>>, t_min : f64, t_max : f64) -> (bool, HitRecord) {
+        let mut result_record = HitRecord::new();
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for object in objects {
+            // Check if every object hits, taking into account that objects further from 
+            // the closest one (t_max > closest_so_far) won't
+            let (hits, temp_record) = object.hit(&self,  t_min, closest_so_far);
+
+            if hits {
+                hit_anything = true;
+                closest_so_far = temp_record.t;
+                result_record = temp_record;
+            }
         }
+
+        (hit_anything, result_record)
+    }
+
+    pub fn ray_color(&self, objects : &Vec<Box<dyn Hittable>>) -> Color {
+        //if let Some(t) = self.hits_sphere(Point {x: 0.0, y: 0.0,z: -1.0,},0.5) {
+        let (hits, hit_record) = self.hits_any_object(objects, 0.0, f64::INFINITY);
+
+        if hits {
+            return Color{x: hit_record.normal.x+1.0, y: hit_record.normal.y+1.0, z: hit_record.normal.z+1.0} * 0.5;
+        }
+
         /*
             Blends white and blue depending on the height of the y coordinate
             after scaling the ray direction to unit length (so −1.0 < y < 1.0)
@@ -54,7 +69,7 @@ impl Ray {
             } * t)
     }
 
-    pub fn hits_sphere(&self, center: Point, radius: f64) -> bool {
+    pub fn hits_sphere(&self, center: Point, radius: f64) -> Option<f64> {
         /*
             Eq. for sphere at center C(C_x, C_y, C_z):
                 (x − C_x)^2 + (y − C_y)^2 + (z − C_z)^2 = R2
@@ -72,15 +87,21 @@ impl Ray {
                 t^2*B^2 + 2*t*B * (A-C) + (A-C)*(A-C) - R^2 = 0
 
 
+            Optimized so that multiplications and divisions by two are removed in the quadratic formula
         */
 
         let oc: Vec3 = self.orig - center; // (A-C)
 
-        let a = Vec3::dot(&self.dir, self.dir); // (t^2*B^2)
-        let b = 2.0 * Vec3::dot(&oc, self.dir); // 2*((A-C)*(t*B))
-        let c = Vec3::dot(&oc, oc) - (radius * radius); // (A-C)*(A-C) - R^2
+        let a = self.dir.length_squared(); //Vec3::dot(&self.dir, self.dir); // (t^2*B^2)
+        let half_b = Vec3::dot(&oc, &self.dir); //2.0 * Vec3::dot(&oc, self.dir); // 2*((A-C)*(t*B))
+        let c = oc.length_squared() - radius*radius; //Vec3::dot(&oc, oc) - (radius * radius); // (A-C)*(A-C) - R^2
 
-        let discriminant = b * b - 4.0 * a * c;
-        discriminant > 0.0 // If it's positive, it has two roots and as such it goes through the sphere
+        let discriminant = half_b * half_b -  a * c;
+
+        if discriminant < 0.0 { // No solution (roots)
+            return None
+        } else {
+            return Some((-half_b - f64::sqrt(discriminant)) / a);
+        }
     }
 }
