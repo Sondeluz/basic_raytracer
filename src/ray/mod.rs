50dae7a868eee459867a15d6c5c2ee0ff991569a
Sam <git@samueldgv.com>
2022-08-21 19:01:19 +0200

    Fix conflicts

--- src/ray/mod.rs
+++ src/ray/mod.rs
@@ -82,40 +82,4 @@ impl Ray {
                 z: 1.0,
             } * t)
     }
-
-    pub fn hits_sphere(&self, center: Point, radius: f64) -> Option<f64> {
-        /*
-            Eq. for sphere at center C(C_x, C_y, C_z):
-                (x − C_x)^2 + (y − C_y)^2 + (z − C_z)^2 = R2
-
-            In vector terms, for a point P(x, y, z):
-                (P - C)*(P-C) = R^2
-
-            We want to know if a ray P(t) = A + tB satisfies the equation (hits the sphere):
-                (P(t)-C)*(P(t)-C) = R^2
-
-            Expanding P(t):
-                (A + tB - C)*(A + tB - C) = R^2
-
-            Expanding further and creating an equation:
-                t^2*B^2 + 2*t*B * (A-C) + (A-C)*(A-C) - R^2 = 0
-
-
-            Optimized so that multiplications and divisions by two are removed in the quadratic formula
-        */
-
-        let oc: Vec3 = self.orig - center; // (A-C)
-
-        let a = self.dir.length_squared(); //Vec3::dot(&self.dir, self.dir); // (t^2*B^2)
-        let half_b = Vec3::dot(&oc, &self.dir); //2.0 * Vec3::dot(&oc, self.dir); // 2*((A-C)*(t*B))
-        let c = oc.length_squared() - radius*radius; //Vec3::dot(&oc, oc) - (radius * radius); // (A-C)*(A-C) - R^2
-
-        let discriminant = half_b * half_b -  a * c;
-
-        if discriminant < 0.0 { // No solution (roots)
-            return None
-        } else {
-            return Some((-half_b - f64::sqrt(discriminant)) / a);
-        }
-    }
 }
